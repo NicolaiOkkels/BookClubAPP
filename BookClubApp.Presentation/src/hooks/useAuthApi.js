ee8af7eb9ae0cc3643fb916995ea4aff68e87c79
Nicolai O. <nico902t@stud.kea.dk>
2024-01-03 22:57:02 +0100

    change env location to fix error

--- BookClubApp.Presentation/src/hooks/useAuthApi.js
+++ BookClubApp.Presentation/src/hooks/useAuthApi.js
@@ -2,9 +2,10 @@ import { useAuth0 } from '@auth0/auth0-react';
 import axios from 'axios';
 import { useMemo, useCallback } from 'react';
 
+const apiUrl = process.env.REACT_APP_API_URL;
+
 const useAuthApi = () => {
   const { getAccessTokenSilently } = useAuth0();
-  const apiUrl = process.env.REACT_APP_API_URL;
 
   const apiClient = useMemo(() => axios.create({
     baseURL: apiUrl,
